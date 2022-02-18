7dc06e50199b7b1a0f4d1ef16b2e6b24085b2961
Blake Miner <miner.blake@gmail.com>
2022-02-17 23:08:41 -0500

    Fixed some bugs and added common google/protobuf imports

--- ProtoTransformer.js
+++ ProtoTransformer.js
@@ -1,18 +1,73 @@
-import { Transformer } from "@parcel/plugin"
-import { parse } from "protocol-buffers-schema"
-import { raw } from "pbf/compile"
+const { readFile } = require("fs/promises")
+const path = require("path")
+const { Transformer } = require("@parcel/plugin")
+const { parse } = require("protocol-buffers-schema")
+const { raw } = require("pbf/compile")
 
-export default new Transformer({
+const cachedImports = {}
+
+// getImport reads and parses the google/protobuf/*.proto import file and
+// subsequently caches it in memory
+async function getImport(name) {
+  if (!cachedImports[name]) {
+    const parsedProto = parse(
+      await readFile(path.join(__dirname, `/imports/${name}.proto`))
+    )
+    cachedImports[name] = parsedProto
+  }
+  return cachedImports[name]
+}
+
+module.exports = new Transformer({
   async transform({ asset }) {
     // Retrieve the asset's source code
     let source = await asset.getCode()
-    // Sadly source maps are not supported by pbf
     // let sourceMap = await asset.getMap()
+    // Sadly source maps are not supported by pbf
 
     // Use protocol-buffers-schema to parse the *.proto file
     const parsed = parse(source)
+
+    // Inject common google/protobuf imports
+    // See files in `imports/*.proto`
+    const messageTypeMap = {}
+    await Promise.all(
+      parsed.imports.map(async filename => {
+        const match = filename.match(
+          /^google\/protobuf\/([a-zA-Z_\d]+)\.proto$/
+        )
+
+        if (match) {
+          const [, name] = match
+          try {
+            const { package, messages } = await getImport(name)
+
+            // Change each message `name` to include package name
+            const importMessages = messages.map(msg => {
+              const newName = package.replace(".", "_") + "_" + msg.name
+              messageTypeMap[package + "." + msg.name] = newName
+              return Object.assign({}, msg, { name: newName })
+            })
+
+            // Change `type` for existing messages
+            parsed.messages.forEach(msg => {
+              msg.fields.forEach(f => {
+                if (messageTypeMap[f.type]) {
+                  f.type = messageTypeMap[f.type]
+                }
+              })
+            })
+
+            // Add import directly to `parsed.messages`
+            parsed.messages = parsed.messages.concat(importMessages)
+          } catch (e) {}
+        }
+      })
+    )
+
     // Use pbf `compileRaw` in `compile.js`
     const code = raw(parsed)
+    asset.type = "js"
     asset.setCode(code)
     // asset.setMap(map)
 
